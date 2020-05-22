df47ed1e18ba2133a2fa089187dfa1180dfd33f4
Jason Peng <penglongxiang1988@gmail.com>
2020-05-22 20:27:42 +0800

    add test_tool_modbus

---
+++ xarm_api/test/test_tool_modbus.cpp
@@ -0,0 +1,27 @@
+#include "ros/ros.h"
+#include "xarm_ros_client.h"
+#include <xarm_driver.h>
+
+// Please run "export ROS_NAMESPACE=/xarm" first
+
+int main(int argc, char **argv)
+{
+	ros::init(argc, argv, "xarm_modbus");
+	ros::NodeHandle nh;
+
+	xarm_api::XArmROSClient client;
+	client.init(nh);
+	int recv_bytes = 7;
+	unsigned char send_data[6] = {0x01,0x06,0x00,0x0A,0x00,0x03}, recv_data[recv_bytes]={0};
+	
+	xarm_msgs::SetToolModbus set_modbus_msg_;
+
+	int ret = client.send_tool_modbus(send_data, 6, recv_data, recv_bytes);
+	fprintf(stderr, "ret = %d, recv_data: ", ret);
+	for(int i=0; i<recv_bytes; i++)
+	{
+		fprintf(stderr, "%x\t", recv_data[i]);
+	}
+	fprintf(stderr, "\n");
+	return 0;
+}
