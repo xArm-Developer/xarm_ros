2219a475175118c2f843286f3fa5137bf8dc182e
Jason Peng <penglongxiang1988@gmail.com>
2020-09-07 19:48:50 +0800

    add example for servo_cartesian service calling

--- xarm_api/test/servo_cartesian_test.cpp
+++ xarm_api/test/servo_cartesian_test.cpp
@@ -1,13 +1,13 @@
-/* Copyright 2018 UFACTORY Inc. All Rights Reserved.
+/* Copyright 2020 UFACTORY Inc. All Rights Reserved.
  *
  * Software License Agreement (BSD License)
  *
- * Author: waylon <weile.wang@ufactory.cc>
+ * Author: jason <jason@ufactory.cc>
  ============================================================================*/
 #include "ros/ros.h"
 #include <xarm_driver.h>
 
-int go_home_test(xarm_msgs::Move srv, ros::ServiceClient client)
+int go_home_test(xarm_msgs::Move &srv, ros::ServiceClient &client)
 {
     srv.request.mvvelo = 20.0 / 57.0;
     srv.request.mvacc = 1000;
@@ -24,17 +24,18 @@ int go_home_test(xarm_msgs::Move srv, ros::ServiceClient client)
     return 0;
 }
 
-int servoj_test(xarm_msgs::Move srv, ros::ServiceClient client)
+int servo_cart_test(xarm_msgs::Move &srv, ros::ServiceClient &client)
 {
-    std::vector<float> joint[2] = {{0, 0, 0, 0, 0, 0, 0}, 
-                                 {0, -0.3, 0, 0, 0, 0, 0}};
+    std::vector<float> inc = {0.3, 0, 0, 0, 0, 0}; 
 
     srv.request.mvvelo = 0;
     srv.request.mvacc = 0;
-    srv.request.mvtime = 0;
-    for (int i = 0; i < 2; i++) 
+    srv.request.mvtime = 1;
+
+    srv.request.pose = inc;
+
+    for (int i = 0; i < 500; i++) 
     {
-        srv.request.pose = joint[i];
         if(client.call(srv))
         {
             ROS_INFO("%s\n", srv.response.message.c_str());
@@ -44,38 +45,11 @@ int servoj_test(xarm_msgs::Move srv, ros::ServiceClient client)
             ROS_ERROR("Failed to call service move_servoj");
             return 1;
         }
-        sleep(2);
+        usleep(1e4); // 0.01s
     }
     return 0;
 }
 
-int move_lineb_test(xarm_msgs::Move srv, ros::ServiceClient client)
-{
-	std::vector<float> pose[5] = {  {300, 0, 100, -3.14, 0, 0},
-                                    {300, 100, 100, -3.14, 0, 0},
-                                    {400, 100, 100, -3.14, 0, 0},
-                                    {400, -100, 100, -3.14, 0, 0},
-                                    {300, 0, 100, -3.14, 0, 0}};
-
-    srv.request.mvvelo = 50;
-    srv.request.mvacc = 1000;
-    srv.request.mvtime = 0;
-    srv.request.mvradii = 0;
-    for (int i = 0; i < 5; i++) 
-    {
-        srv.request.pose = pose[i];
-        if(client.call(srv))
-        {
-            ROS_INFO("%s\n", srv.response.message.c_str());
-        }
-        else
-        {
-            ROS_ERROR("Failed to call service move_lineb");
-            return 1;
-        }
-    }
-    return 0;
-}
 
 int main(int argc, char **argv)
 {
@@ -86,14 +60,15 @@ int main(int argc, char **argv)
 	ros::ServiceClient set_mode_client_ = nh.serviceClient<xarm_msgs::SetInt16>("/xarm/set_mode");
 	ros::ServiceClient set_state_client_ = nh.serviceClient<xarm_msgs::SetInt16>("/xarm/set_state");
   	ros::ServiceClient go_home_client_ = nh.serviceClient<xarm_msgs::Move>("/xarm/go_home");
-	ros::ServiceClient move_lineb_client_ = nh.serviceClient<xarm_msgs::Move>("/xarm/move_lineb");
-	ros::ServiceClient move_servoj_client_ = nh.serviceClient<xarm_msgs::Move>("/xarm/move_servoj");
+	ros::ServiceClient servo_cart_client_ = nh.serviceClient<xarm_msgs::Move>("/xarm/move_servo_cart");
 
     xarm_msgs::SetAxis set_axis_srv_;
     xarm_msgs::SetInt16 set_int16_srv_;
     xarm_msgs::Move move_srv_;
     
-    set_axis_srv_.request.id = 8;
+
+    // STEP 1: Motion Enable
+    set_axis_srv_.request.id = 8; // CHECK!
     set_axis_srv_.request.data = 1;
     
     if(motion_ctrl_client_.call(set_axis_srv_))
@@ -106,6 +81,7 @@ int main(int argc, char **argv)
         return 1;
     }
 
+    // STEP 2: Set Mode to 0
     set_int16_srv_.request.data = 0;
     if(set_mode_client_.call(set_int16_srv_))
     {
@@ -117,6 +93,7 @@ int main(int argc, char **argv)
         return 1;
     }  
 
+    // STEP 3: Set State to 0
     set_int16_srv_.request.data = 0;
     if(set_state_client_.call(set_int16_srv_))
     {
@@ -128,16 +105,38 @@ int main(int argc, char **argv)
         return 1;
     }
 
+    // STEP 4: Go to Home Position
     if(go_home_test(move_srv_, go_home_client_) == 1) 
         return 1;
 
-   if(move_lineb_test(move_srv_, move_lineb_client_) == 1)
-        return 1;
+    /*wait for going to home*/
+    sleep(3);
 
-    /*wait for execution to complete*/
-    sleep(15);
-
-    if(go_home_test(move_srv_, go_home_client_) == 1) 
+    // STEP 5: Set Mode to 1
+    set_int16_srv_.request.data = 1;
+    if(set_mode_client_.call(set_int16_srv_))
+    {
+        ROS_INFO("%s\n", set_int16_srv_.response.message.c_str());
+    }
+    else
+    {
+        ROS_ERROR("Failed to call service set_mode");
         return 1;
+    }
+
+    // STEP 6: Set state to 0
+    set_int16_srv_.request.data = 0;
+    if(set_state_client_.call(set_int16_srv_))
+    {
+        ROS_INFO("%s\n", set_int16_srv_.response.message.c_str());
+    }
+    else
+    {
+        ROS_ERROR("Failed to call service set_state");
+        return 1;
+    }
+
+    // STEP 7: Call SERVO_CARTESIAN service in a loop
+    return servo_cart_test(move_srv_, servo_cart_client_);
 
 }
